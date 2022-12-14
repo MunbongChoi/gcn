12a47f2e33b4f27cef23cf155adda09f9335f620
choimunbong <ansqhd7566@naver.com>
2022-12-14 17:31:14 +0900

    disable cuda something

--- pygcn/train.py
+++ pygcn/train.py
@@ -16,7 +16,7 @@ from pygcn.models import GCN
 parser = argparse.ArgumentParser()
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='Disables CUDA training.')
-parser.add_argument('--fastmode', action='store_true', default=True,
+parser.add_argument('--fastmode', action='store_true', default=False,
                     help='Validate during training pass.')
 parser.add_argument('--seed', type=int, default=42, help='Random seed.')
 parser.add_argument('--epochs', type=int, default=200000,
